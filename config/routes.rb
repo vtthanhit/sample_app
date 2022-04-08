b1fa3762307b970fc459ed749267bd7be905882b
vtthanhit <vtthanh.cit@gmail.com>
2022-04-08 08:58:41 +0700

    Create static pages - use i18n

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope "(:locale)", locale: /en|vi/ do
+    get "static_pages/home"
+    get "static_pages/help"
+  end
 end
