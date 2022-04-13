aa4d3087cd522e5d51f2cbb6406bb6f6dfeae475
vtthanhit <vtthanh.cit@gmail.com>
2022-04-13 10:11:48 +0700

    Chapter_9: Advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,14 @@
 class SessionsController < ApplicationController
+  before_action :find_user, only: :create
+
   def new; end
 
   def create
-    user = User.find_by email: params[:session][:email]
-    if user&.authenticate params[:session][:password]
-      log_in user
+    if @user&.authenticate params[:session][:password]
+      log_in @user
+      params[:session][:remember_me] == "1" ? remember(@user) : forget(@user)
       flash[:success] = t "pages.signin.success"
-      redirect_to user
+      redirect_to @user
     else
       flash.now[:danger] = t "pages.signin.danger"
       render :new
@@ -16,5 +18,14 @@ class SessionsController < ApplicationController
   def destroy
     log_out
     redirect_to static_pages_home_path
+  end
+
+  private
+  def find_user
+    @user = User.find_by email: params.dig(:session, :email)
+    return if @user
+
+    flash.now[:danger] = t "pages.signin.not_exist_email"
+    render :new
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
 
   validates :email, format: {with: VALID_EMAIL_REGEX}, presence: true,
@@ -7,4 +9,28 @@ class User < ApplicationRecord
   validates :password, length: {in: Settings.length_8..Settings.length_20}
   validates :name, presence: true
   has_secure_password
+
+  class << self
+    def digest string
+      cost = BCrypt::Engine::DEFAULT_COST
+      BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_column :remember_digest, User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update_column :remember_digest, nil
+  end
 end
